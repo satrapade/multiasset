d12f57e14b977c53c033e662b7eecda204149e0d
nassuphis <nassuphis@users.noreply.github.com>
2018-07-09 18:27:15 +0100

    Add files via upload

--- test/test_nn_table.R
+++ test/test_nn_table.R
@@ -11,20 +11,60 @@ require(Rblpapi)
 
 rcon<-Rblpapi::blpConnect()
 
-nn_field_seq_eval<-function(x){
-  envir<-parent.frame()
+render_df<-function(x,use_format=1){
+  rdf<-data.table(x)
+  for(i in names(x)){
+    current_format<-attributes(x[[i]])$format
+    if(!is.null(current_format))if(class(current_format)=="list"){
+      rdf[[i]]<-do.call(what=current_format[[use_format]],args=list(x[[i]]))
+    }
+  }
+  rdf
+}
+
+print.nntable<-function(x,...){
+  df<-render_df(x)
+  print(df,...)
+}
+
+set_format<-function(
+  df,
+  fn="*", # all fields
+  ff=.%>%comma(digits=0) # add commas to large numbers
+){
+  table_fields<-names(df)
+  matched_table_fields<-table_fields[grepl(fn,table_fields)]
+  if(length(matched_table_fields)<1)return(df)
+  for(i in matched_table_fields){
+    current_format<-attributes(df[[i]])$format
+    new_format<-c(current_format,ff)
+    attributes(df[[i]])$format<-new_format
+  }
+  if(!"nntable" %in% class(df)){
+    class(df)<-c("nntable",class(df))
+  }
+  df
+}
+
+
+nn_eval_byrow<-function(x){
   x_exp<-mapply(parse,text=x,SIMPLIFY = FALSE)
-  x_res<-mapply(function(e,rownum){
-    current_row<-mapply(
-      function(n,i)get(n,envir)[[i]],
-      n=ls(envir),
-      MoreArgs=list(i=rownum),
-      SIMPLIFY=FALSE
-    )
-    try(eval(e,envir=list2env(current_row)),silent=TRUE)
+  x_res<-mapply(function(e){
+    try(eval(e),silent=TRUE)
   },e=x_exp,rownum=seq_along(x_exp),SIMPLIFY = FALSE)
   x_res
 }
+
+row_wise_fun<-function(...){
+  args<-mapply(
+    eval,
+    tail(as.list(match.call(expand.dots=TRUE)),-1),
+    MoreArgs=list(envir=parent.frame()),
+    SIMPLIFY = FALSE
+  )
+  args
+}
+
 
 df<-fread(input="
     index      | name                             | start        | end
@@ -32,20 +72,11 @@ df<-fread(input="
     SXXP Index | Rblpapi::bdp(index,'NAME')$NAME  | '2017-01-01' | gsub(' Index','',index)
 ",sep="|") %>% 
   set_format("start",list(.%>%paste0("XXX"))) %>%
-  set_format("name",list(bdp=nn_field_seq_eval)) %>%
-  set_format("end",list(subst=nn_field_seq_eval))
+  set_format("name",list(bdp=nn_eval_byrow)) %>%
+  set_format("end",list(subst=nn_eval_byrow)) %>%
+  render_df 
 
-render_df<-function(x,use_format=1){
-  rdf<-data.table(x)
-  edf<-list2env(rdf)
-  for(i in names(x)){
-    current_format<-attributes(x[[i]])$format
-    if(!is.null(current_format))if(class(current_format)=="list"){
-      rdf[[i]]<-do.call(what=current_format[[use_format]],args=list(x[[i]]),envir=edf)
-    }
-  }
-  rdf
-}
+
 
 df1<-data.table(
   index=c("SX7P Index","SXXP Index","UKX Index"),
@@ -56,20 +87,8 @@ df1<-data.table(
   set_format("history",list(bdh=nn_field_seq_eval)) %>%
   render_df
 
-print.nntable<-function(x,...){
-  df<-render_df(x)
-  print(df)
-}
 
-f1<-function(...){
-  args<-mapply(
-    eval,
-    tail(as.list(match.call(expand.dots=TRUE)),-1),
-    MoreArgs=list(envir=parent.frame()),
-    SIMPLIFY = FALSE
-  )
-  args
-}
+
   
 df<-data.table(
   x=1:5,
@@ -82,6 +101,9 @@ do.call(mapply,c(list(FUN=f1,SIMPLIFY=FALSE),df))
 
 
 
-
+f2<-function(x){
+  res<-substitute(x,environment())
+  eval(res)
+}
 
 

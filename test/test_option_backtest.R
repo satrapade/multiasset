893830747ecc31d61751bf99d1d833933ce51148
nassuphis <nassuphis@users.noreply.github.com>
2018-08-29 18:55:01 +0300

    Add files via upload

--- test/test_option_backtest.R
+++ test/test_option_backtest.R
@@ -21,15 +21,48 @@ source("https://raw.githubusercontent.com/satrapade/pairs/master/sql_tools/make_
 source("https://raw.githubusercontent.com/satrapade/utility/master/nn_cast.R")
 source("https://raw.githubusercontent.com/satrapade/pairs/master/utility/make_date_range.R")
 
-# the multi-asset database
-dbma<-dbConnect(
-odbc::odbc(),
-.connection_string = paste0(
-"driver={SQL Server};",
-"server=SQLS071FP\\MULTIASSET;",
-"database=MultiAsset;",
-"trusted_connection=true"
-))
+
+make_market<-function(id="108105",vol_table="VOLATILITY_SURFACE_2014")
+{
+  dbma<-dbConnect(
+    odbc::odbc(),
+    .connection_string = paste0(
+    "driver={SQL Server};",
+    "server=SQLS071FP\\MULTIASSET;",
+    "database=MultiAsset;",
+    "trusted_connection=true"
+  ))
+  the_query<-make_query(
+    security_id=id,
+    the_table=vol_table,
+    query_string = "
+      SELECT 
+        --R{the_table}--.Date AS Date,
+        --R{the_table}--.Days AS Days,
+        --R{the_table}--.Strike AS Strike,
+        --R{the_table}--.Delta AS Delta,
+        --R{the_table}--.ImpliedVol AS ImpliedVol,
+        SECURITY_PRICE.ClosePrice AS ClosePrice
+      FROM --R{the_table}--
+      LEFT JOIN SECURITY_PRICE 
+      ON SECURITY_PRICE.SecurityID = --R{the_table}--.SecurityID
+      AND SECURITY_PRICE.Date = --R{the_table}--.Date
+      WHERE --R{the_table}--.SecurityID = --R{security_id}--
+  ")
+  vol_df<-query(the_query,db=dbma)
+  dbDisconnect(dbma)
+  x<-vol_df[Delta==80]
+  x$Strike<-0
+  y<-vol_df[Delta==(-80)]
+  y$Strike<-max(y$Strike)*10
+  z<-vol_df[Days==30]
+  z$Days<-0
+  res<-rbind(vol_df,x,y,z)
+  res$Date<-fastPOSIXct(res$Date)
+  res
+}
+
+
 
 
 
@@ -56,7 +89,7 @@ make_date_df<-function(start,end){
 make_dates<-function(filter_list,market_df){
   res<-Reduce(function(a,b)eval(bquote(.(a)[.(b)])),filter_list,init=market_df)
   res$start<-res$pday
-  res$end<-c(res$pday[-1],nrow(market_df))
+  res$end<-c(res$pday[-1],max(market_df$pday))
   res
 }
 
@@ -69,7 +102,7 @@ EC <- function(S,X,t,r,v)
   S*pnorm(d1)-X*exp(-r*t)*pnorm(d2)
 }
 
-# option models
+
 EP  <- function(S,X,t,r,v)
 {
   d1 <- (log(S/X)+(r+0.5*v^2)*t)/(v*sqrt(t))
@@ -77,14 +110,17 @@ EP  <- function(S,X,t,r,v)
   X*exp(-r*t)*pnorm(-d2)-S*pnorm(-d1)
 }
 
-
+# resamples volgrid to specified strikes, maturities on a single date
 resample_vol_grid<-function (vol_df,strikes,maturities) 
 {
   strikes_in<-sort(unique(vol_df$Strike))
   maturities_in<-sort(unique(vol_df$Days))
   j<-data.table::frank(vol_df$Strike,ties.method = "dense")
   i<-data.table::frank(vol_df$Days,ties.method = "dense")
-  vol_grid<-sparseMatrix(j=j,i=i,x=vol_df$ImpliedVol)
+  vol_grid<-t(apply(sparseMatrix(j=j,i=i,x=vol_df$ImpliedVol,use.last.ij=TRUE),1,function(a){
+    ndx<-which(a>0)
+    approx(x=ndx,y=a[ndx],xout=seq_along(a),yleft=max(a[ndx]),yright=min(a[ndx]))$y
+  }))
   res<-bilinear(
     y=strikes_in,
     x=maturities_in,
@@ -92,40 +128,18 @@ resample_vol_grid<-function (vol_df,strikes,maturities)
     y0=rep(strikes,times=length(maturities)),
     x0=rep(maturities,each=length(strikes))
   )
-  data.table(Strike=res$y,Days=res$x,ImpliedVol=res$z)
+  data.table(Strike=res$y,Days=res$x,ImpliedVol=res$z,ClosePrice=vol_df$ClosePrice[1])
 }
 
 
-on_site<-TRUE
+#
+# fetch vol surface
+#
+
+on_site<-FALSE
 
 if(on_site){
-  spx_vol <- query(make_query(
-  security_id="108105",
-  query_string = "
-    SELECT 
-      VOLATILITY_SURFACE_2014.Date AS Date,
-      VOLATILITY_SURFACE_2014.Days AS Days,
-      VOLATILITY_SURFACE_2014.Strike AS Strike,
-      VOLATILITY_SURFACE_2014.Delta AS Delta,
-      VOLATILITY_SURFACE_2014.ImpliedVol AS ImpliedVol,
-      SECURITY_PRICE.ClosePrice AS ClosePrice
-    FROM VOLATILITY_SURFACE_2014
-    LEFT JOIN SECURITY_PRICE 
-    ON SECURITY_PRICE.SecurityID = VOLATILITY_SURFACE_2014.SecurityID
-    AND SECURITY_PRICE.Date = VOLATILITY_SURFACE_2014.Date
-    WHERE VOLATILITY_SURFACE_2014.SecurityID = --R{security_id}--
-  "),
-  db=dbma) %>% {
-    x<- .[Delta==80]
-    x$Strike<-9999
-    y<- .[Delta==(-80)]
-    y$Strike<-0
-    z<- .[Days==30]
-    z$Days<-0
-    res<-rbind(.,x,y,z)
-    res$Date<-fastPOSIXct(res$Date)
-    res
-  }
+  spx_vol <- make_market(id="108105",vol_table="VOLATILITY_SURFACE_2014")
   fwrite(spx_vol,"spx_vol.csv")
 } else {
   spx_vol<-fread("spx_vol.csv")
@@ -133,10 +147,16 @@ if(on_site){
 }
 
 maturities<-sort(unique(spx_vol$Days))
-new_strikes<-seq(from=1000,to=3000,length.out=50)
-resampled_spx_vol<-spx_vol[,resample_vol_grid(.SD,new_strikes,maturities),keyby=Date]
+strikes<-seq(from=1000,to=3000,length.out=50)
+resampled_spx_vol<-spx_vol[,resample_vol_grid(.SD,strikes,maturities),keyby=Date]
 
-market_df<- make_date_df(as.character(min(spx_vol$Date)),as.character(max(spx_vol$Date))) 
+
+
+
+market_df<- make_date_df(
+  start=as.character(min(spx_vol$Date)),
+  end=as.character(max(spx_vol$Date))
+)[date %in% spx_vol$Date]
 
 
 # criteria for roll dates
@@ -146,14 +166,17 @@ expiries<-list(
   quote(wday_count==3)
 )
 
-
 roll_dates<-make_dates(expiries,market_df)
+
 roll_dates$strike<-merge(
   x=roll_dates,
   y=spx_vol[,.(close=ClosePrice[1]),keyby=Date],
   by.x=c("date"),
   by.y=c("Date")
-)$close*1.1
+)$close*0.95
+
+
+
 
   
 # create portfolio 
@@ -163,54 +186,53 @@ ptf<-data.table(
   roll=findInterval(market_df$pday,roll_dates$pday,rightmost.closed = FALSE,all.inside = FALSE)
 )[roll>0 & roll<nrow(roll_dates)][,c(.SD,list(
   strike=roll_dates$strike[roll],
-  days=roll_dates$end[roll]-market_df$pday[day]
+  start=roll_dates$start[roll],
+  end=roll_dates$end[roll],
+  days=roll_dates$end[roll]-day
 ))][,c(.SD,list(
-  lo_strike=new_strikes[findInterval(strike,new_strikes)],
-  hi_strike=new_strikes[findInterval(strike,new_strikes)+1],
+  lo_strike=strikes[findInterval(strike,strikes)],
+  hi_strike=strikes[findInterval(strike,strikes)+1],
   lo_mat=maturities[findInterval(days,maturities)],
   hi_mat=maturities[findInterval(days,maturities)+1]
 ))]
 
 
+interpolate_vol<-function(ptf,vsurf)
+{
+  strikes<-sort(unique(vsurf$Strike))
+  maturities<-sort(unique(vsurf$Days))
+  option_dets<-data.table(
+    date=ptf$date,
+    lo_strike=strikes[findInterval(ptf$strike,strikes)],
+    hi_strike=strikes[findInterval(ptf$strike,strikes)+1],
+    lo_mat=maturities[findInterval(ptf$days,maturities)],
+    hi_mat=maturities[findInterval(ptf$days,maturities)+1]
+  )
+  vol_ll<-merge(x=option_dets,y=vsurf,by.x=c("date","lo_strike","lo_mat"),by.y=c("Date","Strike","Days"))$ImpliedVol
+  vol_lh<-merge(x=option_dets,y=vsurf,by.x=c("date","lo_strike","hi_mat"),by.y=c("Date","Strike","Days"))$ImpliedVol
+  vol_hl<-merge(x=option_dets,y=vsurf,by.x=c("date","hi_strike","lo_mat"),by.y=c("Date","Strike","Days"))$ImpliedVol
+  vol_hh<-merge(x=option_dets,y=vsurf,by.x=c("date","hi_strike","hi_mat"),by.y=c("Date","Strike","Days"))$ImpliedVol
+  t_strike<-(ptf$strike-ptf$lo_strike)/(ptf$hi_strike-ptf$lo_strike)
+  t_mat<-(ptf$days-ptf$lo_mat)/(ptf$hi_mat-ptf$lo_mat)
+  rowSums(cbind(
+    ptf$vol_ll*(1-ptf$t_strike)*(1-ptf$t_mat),
+    ptf$vol_hl*(ptf$t_strike)*(1-ptf$t_mat),
+    ptf$vol_lh*(1-ptf$t_strike)*(ptf$t_mat),
+    ptf$vol_hh*(ptf$t_strike)*(ptf$t_mat)
+  ))
+}
 
-ptf$vol_ll<-merge(x=ptf,y=resampled_spx_vol,by.x=c("date","lo_strike","lo_mat"),by.y=c("Date","Strike","Days"))$ImpliedVol
-ptf$vol_lh<-merge(x=ptf,y=resampled_spx_vol,by.x=c("date","lo_strike","hi_mat"),by.y=c("Date","Strike","Days"))$ImpliedVol
-ptf$vol_hl<-merge(x=ptf,y=resampled_spx_vol,by.x=c("date","hi_strike","lo_mat"),by.y=c("Date","Strike","Days"))$ImpliedVol
-ptf$vol_hh<-merge(x=ptf,y=resampled_spx_vol,by.x=c("date","hi_strike","hi_mat"),by.y=c("Date","Strike","Days"))$ImpliedVol
-ptf$t_strike<-(ptf$strike-ptf$lo_strike)/(ptf$hi_strike-ptf$lo_strike)
-ptf$t_mat<-(ptf$days-ptf$lo_mat)/(ptf$hi_mat-ptf$lo_mat)
 
-ptf$vol <-  ptf$vol_ll*(1-ptf$t_strike)*(1-ptf$t_mat) + 
-            ptf$vol_hl*(ptf$t_strike)*(1-ptf$t_mat) +
-            ptf$vol_lh*(1-ptf$t_strike)*(ptf$t_mat) + 
-            ptf$vol_hh*(ptf$t_strike)*(ptf$t_mat)
+
+ptf$vol <-  interpolate_vol(ptf,resampled_spx_vol)
 
 ptf$yfrac<-ptf$days/365
 
+ptf$close<-merge(x=ptf,y=resampled_spx_vol,by.x=c("date","hi_strike","hi_mat"),by.y=c("Date","Strike","Days"))$ClosePrice
 
 
-ptf$EC<-EC(ptf$px,ptf$strike,ptf$yfrac,0,ptf$vol)
-ptf$EP<-EP(ptf$px,ptf$strike,ptf$yfrac,0,ptf$vol)
-
-
-
-# fast resampling
-
-
-
-
-
-
-resampled_spx_vol[Date==fastPOSIXct("2014-01-02")]
-spx_vol[Date==fastPOSIXct("2014-01-02")]
-
-
-
-
-
-
-
-
+ptf$EC<-EC(ptf$close,ptf$strike,ptf$yfrac,0,ptf$vol)
+ptf$EP<-EP(ptf$close,ptf$strike,ptf$yfrac,0,ptf$vol)
 
 
 

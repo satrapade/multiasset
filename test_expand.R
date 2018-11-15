5d9ef2edd27f59fa311dfbbc5bff2b0d93a729ea
nassuphis <nassuphis@users.noreply.github.com>
2018-11-15 14:44:26 +0000

    Add files via upload

--- test_expand.R
+++ test_expand.R
@@ -1,17 +1,79 @@
+require(magrittr)
+require(brotli)
+source("https://raw.githubusercontent.com/satrapade/utility/master/utility_functions.R")
+
+resampled_spx_vol<-"compressed_resampled_spx_vol.txt" %>% scan(character()) %>% decompress
+resampled_sx5e_vol<-"compressed_resampled_sx5e_vol.txt" %>% scan(character()) %>% decompress
+resampled_ftse_vol<-"compressed_resampled_ftse_vol.txt" %>% scan(character()) %>% decompress
+resampled_nky_vol<-"compressed_resampled_nky_vol.txt" %>% scan(character()) %>% decompress
+#resampled_eem_vol<-"compressed_resampled_eem_vol.txt" %>% scan(character()) %>% decompress
+
+all_vol<-rbind( # data.table with all volatilities
+  data.table(resampled_spx_vol,market="spx"),
+  data.table(resampled_sx5e_vol,market="sx5e"),
+  data.table(resampled_ftse_vol,market="ftse"),
+  data.table(resampled_nky_vol,market="nky")
+)[,.(
+    Date=as.Date(stri_sub(Date,1,10),format="%Y-%m-%d"),
+    Strike=Strike,
+    Days=Days,
+    ImpliedVol=ImpliedVol,
+    ClosePrice=ClosePrice,
+    market=market
+)][,
+  .(
+    Strike=Strike,
+    Days=Days,
+    ImpliedVol=ImpliedVol,
+    ClosePrice=ClosePrice,
+    market=market,
+    market_count=length(market)
+  ),
+  keyby=Date
+][ClosePrice>0]
 
 
-x<-data.table(
-  day=rep(seq(from=1,by=3,length.out=15),each=3),
-  strike=rep(seq(from=1,to=3),times=15),
-  v=runif(15*3)
-)
+#
+# vector of dates -> 
+# data.table of
+#   all dates
+#   rolled date
+#   roll count
+#
+roll_days<-function(dates){
+  diffs<-as.integer(diff(dates))
+  locs<-cumsum(c(1,diffs))
+  times<-c(diffs,1)
+  ndx<-seq(from=1,to=locs[length(locs)],by=1)
+  last_reset<-rep(locs,times=times)
+  rolled_dates<-rep(dates,times=c(diffs,1))
+  sequential_dates<-seq(from=dates[1],to=dates[length(dates)],by=1)
+  data.table(
+    allDate=sequential_dates,
+    Date=rolled_dates,
+    roll_count=ndx-last_reset
+  )
+}
 
-x1<-min(x[strike==1][,day])
-x2<-max(x[strike==1][,day])
+#
+#
+#
+roll_vols<-function(vsurf){
+  vsurf[
+    roll_days(sort(unique(vsurf$Date))),
+    on="Date", 
+    allow.cartesian=TRUE
+  ][,.(
+    Date=allDate,
+    Strike=Strike,
+    Days=Days,
+    ImpliedVol=ImpliedVol,
+    ClosePrice=ClosePrice,
+    market=market,
+    market_count=market_count,
+    roll_count=roll_count
+  )]
+}
 
-i<-findInterval(seq(from=x1,to=x2),x[strike==1,day])
+spx_vol<-all_vol[market=="spx"]
 
-data.table(
-  from=x[strike==1,day][i],
-  to=seq(x1,x2)
-)
